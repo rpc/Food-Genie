a2ce892a788a01109a781b63938cb0e23f4a3812
Roger <justnyeh@gmail.com>
2011-12-16 15:05:53 +0000

    all proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodgenie::Application.config.secret_token = 'e3ce7c61f75ae9f897d25b2060803f4937dd8f552815851f7d426034a19a95e09189100abe36ae29e0198fa4f3ea3c4b03ee726267dc2f378841dfd2492a8aed'
